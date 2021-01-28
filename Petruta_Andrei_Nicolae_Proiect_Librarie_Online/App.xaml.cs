fa20d6b7d92c0e2e10bb3b64b0c404962356e974
Andrei Petruta <aandrei.petruta@gmail.com>
2021-01-28 18:19:28 +0200

    Add project files.

---
+++ Petruta_Andrei_Nicolae_Proiect_Librarie_Online/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Petruta_Andrei_Nicolae_Proiect_Librarie_Online
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
